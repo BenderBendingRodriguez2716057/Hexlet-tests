f7a7a03edae71cdfa7a419cb3e7cbe823f299091
Sergey <sereganorilsk89@gmail.com>
2023-07-07 21:05:16 +0500

    changes

---
+++ happyTicket.php
@@ -0,0 +1,20 @@
+function isHappy(string $num) {
+    $numStr = (string) $num;
+    $count = strlen($numStr);
+    $half = $count / 2;
+
+    $i = 0;
+    $sumBegin = 0;
+    while ($i <= $half - 1) {
+        $sumBegin = $sumBegin + $numStr[$i];
+        $i += 1;
+    }
+
+    $i = -1;
+    $sumEnd = 0;
+    while ($i >= -$half) {
+        $sumEnd += $numStr[$i];
+        $i -= 1;
+    }
+    return $sumBegin === $sumEnd;
+}
